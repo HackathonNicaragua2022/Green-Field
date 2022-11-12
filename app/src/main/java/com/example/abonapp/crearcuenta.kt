38fd09b6a228f699c17f76aab58f211e1ff69f61
dreghal <halforddreg@gmail.com>
2022-11-12 03:03:52 +0100

    inicio de proyecto

---
+++ app/src/main/java/com/example/abonapp/crearcuenta.kt
@@ -0,0 +1,11 @@
+package com.example.abonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class crearcuenta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crearcuenta)
+    }
+}
